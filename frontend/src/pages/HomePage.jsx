9d0e454f4a3c65bb911cd802a2f2780363c48659
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-23 14:54:00 -0700

    Changed width of HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-base-200">
       <NavBar />
-      <div className="max-w-7xl mx-auto p-4 mt-6">
+      <div className="max-w-6xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Reels</div>}
         {reels.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
