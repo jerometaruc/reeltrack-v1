387072597618b807c7824cca5ed17da9fdff5229
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-23 16:49:31 -0700

    Added ClapperboardIcon to NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { PlusIcon } from 'lucide-react';
+import { PlusIcon, ClapperboardIcon } from 'lucide-react';
 
 const NavBar = () => {
   return (
     <header className="bg-base-300 border-b border-base-content/10">
         <div className="mx-auto max-w-6xl p-4">
             <div className="flex items-center justify-between">
-                <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
-                    ReelTrack
+                <h1 className="flex items-center gap-2 text-3xl font-bold text-primary font-mono tracking-tight">
+                ReelTrack
+                <ClapperboardIcon className="size-8 text-primary" />
                 </h1>
                 <div className="flex items-center gap-4">
                     <Link to={"/add"} className="btn btn-primary">
