51a778f80a553d733da86ccd14b4cc185ecdeee7
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-23 15:38:20 -0700

    Added Post functionality to AddPage

--- frontend/src/pages/AddPage.jsx
+++ frontend/src/pages/AddPage.jsx
@@ -1,29 +1,83 @@
 import React, { useState } from 'react';
 import { ArrowLeftIcon } from 'lucide-react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const AddPage = () => {
   const [title, setTitle] = useState("");
   const [year, setYear] = useState("");
-  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!title.trim() || !year.trim()) {
+      toast.error("All fields are required");
+      return;
+    }
+    setLoading(true);
+    try {
+      await axios.post("http://localhost:3000/api/reels", {
+        title,
+        year
+      });
+      toast.success("Reel created successfully!");
+      navigate("/");
+    } catch (error) {
+      console.log("Error creating reel", error);
+      toast.error("Failed to create note");
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
-        <div className="max-w-2xl mx-auto"></div>
+        <div className="max-w-2xl mx-auto">
           <Link to={"/"} className="btn btn-ghost mb-6">
             <ArrowLeftIcon className="size-5" />
             Back to Reels
           </Link>
           <div className="card bg-base-100">
             <div className="card-body">
-              <h2 className="card-title text-2xl mb-4">Add New Reel</h2>
+              <h2 className="card-title text-2xl mb-4">New Reel</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Movie Title"
+                    className="input input-bordered"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Year</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Release Year"
+                    className="input input-bordered"
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}
+                  />
+                </div>
+                <div className="card-actions justify-end">
+                  <button type="submit" className="btn btn-primary" disabled={loading}>
+                    {loading ? "Adding..." : "Add Reel"}
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
