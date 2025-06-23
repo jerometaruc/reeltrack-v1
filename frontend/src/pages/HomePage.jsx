4d7aa49a04abe6950812322976fa0c4808381c51
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-23 16:54:54 -0700

    Changed logic in HomePage for testing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -31,9 +31,9 @@ const HomePage = () => {
       <div className="max-w-6xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Reels</div>}
 
-        {reels.length > 0 && <ReelsNotFound />}
+        {reels.length === 0 && <ReelsNotFound />}
 
-        {reels.length === 0 && (
+        {reels.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {reels.map((reel) => (
               <ReelCard key={reel._id} reel={reel} setReels={setReels} />
