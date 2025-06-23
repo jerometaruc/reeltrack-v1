0150b694bfed3177d9a3b21d13bc16a37912f813
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-23 16:45:21 -0700

    Added ReelsNotFound component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import NavBar from '../components/NavBar';
 import ReelCard from '../components/ReelCard';
+import ReelsNotFound from "../components/ReelsNotFound";
 
 const HomePage = () => {
   const [reels, setReels] = useState([]);
@@ -29,7 +30,10 @@ const HomePage = () => {
       <NavBar />
       <div className="max-w-6xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Reels</div>}
-        {reels.length > 0 && (
+
+        {reels.length > 0 && <ReelsNotFound />}
+
+        {reels.length === 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {reels.map((reel) => (
               <ReelCard key={reel._id} reel={reel} setReels={setReels} />
